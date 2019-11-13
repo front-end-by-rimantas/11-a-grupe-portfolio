741d4e0346062419c801e6a87a2ac372a0b008a7
Rimantas <belauzas@yahoo.com>
2019-11-13 11:59:43 +0200

    Numbers section rendering with JavaScript #9

--- js/functions.js
+++ js/functions.js
@@ -10,9 +10,43 @@
 
 // numbers
 function renderBlocks( list ) {
-    console.log(list);
+    let HTML = '';
+    let good = 0;
+
+    if ( !Array.isArray(list) ) {
+        return console.error('ERROR: reikia saraso..');
+    }
+
+    for ( let i=0; i<list.length; i++) {
+        const item = list[i];
+        if ( !item.icon ||
+             !item.number ||
+             !item.title ) {
+            continue;
+        }
+        if ( good === 4 ) {
+            break;
+        }
+        // HTML += '<div class="block">\
+        //             <i class="fa fa-'+ item.icon +'"></i>\
+        //             <div class="number">\''+ item.number +'\'</div>\
+        //             <h4>'+ item.title +'</h4>\
+        //         </div>';
+
+        HTML += `<div class="block">
+                    <i class="fa fa-${item.icon}"></i>
+                    <div class="number">${item.number}</div>
+                    <h4>${item.title}</h4>
+                </div>`;
+
+        good++;
+    }
+
+    if ( good === 0 ) {
+        return console.error('ERROR: duotas sarasas, bet arba tuscias, arba nei vieno gero duomens');
+    }
     
-    return;
+    return document.getElementById('achievements').innerHTML = HTML;
 }
 
 // skills
